fed0778b98af84b2ef8e063adad603ea4cd3588c
Jairo Janco <jairorodrigojancomartinez742@gmail.com>
2026-03-31 17:19:04 -0400

    Delete conjetura_collatz directory

--- conjetura_collatz/target/debug/conjetura_collatz.d
+++
@@ -1,1 +0,0 @@
-C:\Users\SOPORTE\OneDrive\Desktop\rust\conjetura_collatz\target\debug\conjetura_collatz.exe: C:\Users\SOPORTE\OneDrive\Desktop\rust\conjetura_collatz\src\main.rs
--- conjetura_collatz/target/debug/deps/conjetura_collatz.d
+++
@@ -1,5 +0,0 @@
-C:\Users\SOPORTE\OneDrive\Desktop\rust\conjetura_collatz\target\debug\deps\conjetura_collatz.d: src\main.rs
-
-C:\Users\SOPORTE\OneDrive\Desktop\rust\conjetura_collatz\target\debug\deps\conjetura_collatz.exe: src\main.rs
-
-src\main.rs:
